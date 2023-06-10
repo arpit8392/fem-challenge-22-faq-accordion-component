370e37c3d54f8792b665b35f9a47819d3a626009
Arpit Namdev <arpit8392@gmail.com>
2023-06-10 16:15:34 +0530

    Base Build

---
+++ components/Accordion.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { Disclosure } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+import { data } from '@/data'
+
+const Accordion = () => {
+	return (
+		<div className='flex flex-col gap-y-8 md:gap-y-11'>
+			<h1 className='self-center text-3xl font-bold text-darkDesaturatedBlue md:self-start md:text-4xl'>
+				FAQ
+			</h1>
+			<dl className='flex flex-col gap-y-3 '>
+				{data.map((faq) => (
+					<Disclosure as='div' key={faq.question}>
+						{({ open }) => (
+							<>
+								<dt>
+									<Disclosure.Button className='flex w-full items-center justify-between'>
+										<span
+											className={`text-left text-sm  text-veryDarkGrayishBlue md:text-base ${
+												open ? 'font-bold' : 'font-normal'
+											}`}>
+											{faq.question}
+										</span>
+										<span className='md:ml-20'>
+											<ChevronDownIcon
+												className={
+													open
+														? 'h-6 w-6 rotate-180 transform text-softRed'
+														: 'h-6 w-6 text-softRed'
+												}
+											/>
+											<span className='sr-only'>Button to show the answer</span>
+										</span>
+									</Disclosure.Button>
+								</dt>
+								<Disclosure.Panel as='dd' className='pt-3 '>
+									<p className='max-w-sm text-xs leading-5 text-darkGrayishBlue md:text-sm md:leading-7'>
+										{faq.answer}
+									</p>
+								</Disclosure.Panel>
+								<hr className='mt-4 h-[1px] bg-lightGrayishBlue' />
+							</>
+						)}
+					</Disclosure>
+				))}
+			</dl>
+		</div>
+	)
+}
+export default Accordion
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				softViolet: 'hsl(273, 75%, 66%)',
+				softBlue: 'hsl(240, 73%, 65%)',
+				darkDesaturatedBlue: 'hsl(238, 29%, 16%)',
+				softRed: 'hsl(14, 88%, 65%)',
+				veryDarkGrayishBlue: 'hsl(237, 12%, 33%)',
+				darkGrayishBlue: 'hsl(240, 6%, 50%)',
+				lightGrayishBlue: 'hsl(240, 5%, 91%)',
+			},
+		},
+	},
+	plugins: [],
 }
